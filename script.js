1c067d922cd153dcdcb7c772b62aabf8f3063f3c
jimmyucartz <jimmypappachan@ucartz.com>
2024-04-01 20:11:19 +0530

    Cases Page , Social icons in top navbar

--- script.js
+++ script.js
@@ -13,6 +13,35 @@ $(() => {
     interval: 6000,
   });
   $(".sect-title").fadeIn(1000);
+
+  $(".case").mouseenter(function () {
+    $(this.children[0].children[0])
+      .fadeIn()
+      .css({ display: "flex", flexDirection: "column" });
+    $(this).css({ opacity: 0.9 });
+  });
+  $(".case").on("touchstart", function () {
+    $(this.children[0].children[0])
+      .fadeIn()
+      .css({ display: "flex", flexDirection: "column" });
+    $(this).css({ opacity: 0.9 });
+  });
+
+  $(".case").on("touchend", function () {
+    $(this.children[0].children[0]).fadeOut();
+    $(this).css({ opacity: 1 });
+  });
+
+  // $(".case").touchstart(function () {
+  //   $(this.children[0].children[0])
+  //     .fadeIn()
+  //     .css({ display: "flex", flexDirection: "column" });
+  //   $(this).css({ opacity: 0.9 });
+  // });
+  $(".case").mouseleave(function () {
+    $(this.children[0].children[0]).fadeOut();
+    $(this).css({ opacity: 1 });
+  });
   let hiddenelements = document.querySelectorAll(".hidden");
   hiddenelements.forEach((el) => observer.observe(el));
 });
